deddf8e97c3209676d0b7e38017d6134cb6fbc44
Jean-Michel Malary <malaryjm@gmail.com>
2019-04-10 19:31:06 -0400

    Hello World Deaf

---
+++ app/src/main/java/com/example/jean_michel/coach/model/Profil.java
@@ -0,0 +1,20 @@
+package com.example.jean_michel.coach.model;
+
+/**
+ * Created by Jean-Michel on 2019-04-10.
+ */
+
+public class Profil {
+
+    // propriétés
+    private Integer poids;
+    private Integer taille;
+    private Integer age;
+    private Integer sexe;
+    private float img;
+    private String message;
+
+
+
+
+}
---
+++ app/src/main/java/com/example/jean_michel/coach/vue/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jean_michel.coach.vue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jean_michel.coach.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
